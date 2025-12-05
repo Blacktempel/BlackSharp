ac0e09fb91c5b27f1d2251e9e32f2e1e43b8d3c2
Blacktempel <6657796+Blacktempel@users.noreply.github.com>
2025-12-05 20:07:49 +0100

    Add interfaces.

---
+++ BlackSharp.Core/Interfaces/ICloneable.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2025 Florian K.
+ *
+ */
+
+namespace BlackSharp.Core.Interfaces
+{
+    /// <inheritdoc cref="ICloneable"/>
+    public interface ICloneable<T>
+    {
+        /// <inheritdoc cref="ICloneable.Clone"/>
+        T Clone();
+    }
+}
---
+++ BlackSharp.Core/Interfaces/ICopyable.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2025 Florian K.
+ *
+ */
+
+namespace BlackSharp.Core.Interfaces
+{
+    /// <summary>
+    /// Defines a method to copy the state from another instance of the same type.
+    /// </summary>
+    public interface ICopyable<T>
+    {
+        /// <summary>
+        /// Copies the values from the specified object to the current instance.
+        /// </summary>
+        /// <remarks>This shall copy value types only, not copying reference types at all.</remarks>
+        void Copy(T from);
+    }
+}
